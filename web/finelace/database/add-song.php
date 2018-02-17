f85f4458863bddf982b90bcc351d7b7e39879654
coryswainston <swainston.cory89@gmail.com>
2018-02-17 01:24:18 -0700

    Fix audio upload

--- web/finelace/database/add-song.php
+++ web/finelace/database/add-song.php
@@ -4,9 +4,7 @@ require 'database.php';
 $target_dir = "../music/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $song_uri = "music/" . basename($_FILES["fileToUpload"]["name"]);
-echo $target_file;
-echo $target_dir;
-$audio_type = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$audio_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
 
 $db = get_db();
@@ -14,7 +12,7 @@ $db = get_db();
 $title = htmlspecialchars($_POST['song_title']);
 $artist = htmlspecialchars($_POST['song_artist']);
 
-$stmt = $db->prepare('INSERT INTO song (songartist, songtitle, songuri) VALUES (:songartist, :songtitle, :songuri)');
+$stmt = $db->prepare('INSERT INTO song (songartist, songtitle, songuri, albumid) VALUES (:songartist, :songtitle, :songuri, 4)');
 $stmt->bindValue(':songtitle', $title);
 $stmt->bindValue(':songartist', $artist);
 $stmt->bindValue(':songuri', $song_uri);
