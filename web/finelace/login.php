1839ef08beff493b31c2fbc385957e200318c518
coryswainston <swainston.cory89@gmail.com>
2018-02-19 18:28:32 -0700

    Fix authentication

--- web/finelace/login.php
+++ web/finelace/login.php
@@ -7,42 +7,44 @@
     <link rel="stylesheet" href="css/login.css">
   </head>
   <body>
-    <?php
-    function finish_login() {
-      header('Location: index.php');
-    }
 
-    if ($_SESSION['login'] == 'admin') {
-      finish_login();
-    }
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-      require 'database/database.php';
-
-      $success = false;
-
-      $email = $_POST['email'];
-      $password = $_POST['password'];
-
-      $db = get_db();
-      foreach ($db->query('SELECT * FROM siteuser') as $user) {
-        if ($user['useremail'] == $email && $user['password'] == $password) {
-          $success = true;
-        }
-      }
-
-      if (!success) {
-        header('Location: login.php?success=false');
-      }
-      else {
-        $_SESSION['login'] = "admin";
-        $_SESSION['username'] = $user['username'];
-        $_SESSION['userid'] = $user['userid'];
-        finish_login();
-      }
-    }
-     ?>
    <main>
+     <?php
+     function finish_login() {
+       header('Location: index.php');
+     }
+
+     if ($_SESSION['login'] == 'admin') {
+       finish_login();
+     }
+
+     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+       require 'database/database.php';
+
+       $success = false;
+
+       $email = $_POST['email'];
+       $password = $_POST['password'];
+
+       $db = get_db();
+       foreach ($db->query('SELECT * FROM siteuser') as $user) {
+         if ($user['useremail'] == $email && $user['userpassword'] == $password) {
+           $success = true;
+           break;
+         }
+       }
+
+       if (!$success) {
+         header('Location: login.php?success=false');
+       }
+       else {
+         $_SESSION['login'] = "admin";
+         $_SESSION['username'] = $user['username'];
+         $_SESSION['userid'] = $user['userid'];
+         finish_login();
+       }
+     }
+     ?>
      <div class="login-form">
        <h2>Admin Login</h2>
        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
