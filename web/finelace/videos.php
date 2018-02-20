120f182769a448a0732013029baf29aadc250131
coryswainston <swainston.cory89@gmail.com>
2018-02-19 18:17:35 -0700

    Add videos page

---
+++ web/finelace/videos.php
@@ -0,0 +1,7 @@
+<?php require 'modules/header.php'; ?>
+
+<main>
+  <iframe class="youtube" src="http://www.youtube.com/embed/?listType=user_uploads&list=fine%20lace" width="480" height="400"></iframe>
+</main>
+
+<?php require 'modules/footer.php'; ?>
